fb31d8acd454a3bf4120b1fbc0daa70a3bbdfdf9
Vladimir <zloienot666@gmail.com>
2022-07-11 22:27:42 +0300

    #1

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int ticketPrice = 2000; // Стоимость  1 билета
         int bonusPrice = 20; // Стоимость 1 мили
-        boolean isPurchase = true;
-
-        int mile;
-        if (isPurchase) {
-            mile = 1;
-        } else {
-            mile = 0;
-        }
-
         int bonus = ticketPrice / bonusPrice ;
 
         System.out.println("Итоговое количество миль : "+ bonus);
